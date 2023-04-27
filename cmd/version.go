165851659d7823de01248b87e82a7bdb93f9cd96
unravel11 <1509410143@qq.com>
2023-04-27 09:05:29 +0800

    complete rhino version,to do:test rhino version command

--- cmd/version.go
+++ cmd/version.go
@@ -18,21 +18,51 @@ package cmd
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+// rhinoVersion is the version of Rhino
+var (
+	rhinoVersion string = "v0.2.0"
+)
+
+// printMpiVersion prints the version of MPI installed on the local machine
+func printMpiVersion() (string, error) {
+	var output []byte
+	var err error
+	var versions []string
+	if _, err = exec.LookPath("ompi_info"); err == nil {
+		output, err = exec.Command("ompi_info", "--version").Output()
+		if err == nil {
+			versions = append(versions, strings.Split(string(output), "\n")[0])
+		}
+	}
+	if _, err = exec.LookPath("mpichversion"); err == nil {
+		output, err = exec.Command("mpichversion").Output()
+		if err == nil {
+			versions = append(versions, strings.Split(string(output), "\n")[0])
+		}
+	}
+	if len(versions) == 0 {
+		return "", fmt.Errorf("neither openmpi nor mpich found")
+	}
+	return strings.Join(versions, "\n"), nil
+}
+
+// NewVersionCommand creates a new version command
 func NewVersionCommand() *cobra.Command {
 	versionCmd := &cobra.Command{
-		Use:   "version",
-		Short: "Print the version of MPI installed on the local machine",
-		Run: func(cmd *cobra.Command, args []string) {
-			output, err := exec.Command("mpirun", "--version").Output()
+		Use:   "version ",
+		Short: "Print the version of Rhino and MPI installed on the local machine",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			mpiVersion, err := printMpiVersion()
 			if err != nil {
-				fmt.Println("Error:", err)
-				return
+				return err
 			}
-			fmt.Printf("MPI version: %s\n", string(output))
+			fmt.Printf("OpenRHINO %s\n%s\n", rhinoVersion, mpiVersion)
+			return nil
 		},
 	}
 	return versionCmd
---
+++ cmd/version_test.go
@@ -0,0 +1,37 @@
+// generate a test for version
+// Path: cmd/version_test.go
+// Compare this snippet from cmd/root_test.go:
+//   - you may not use this file except in compliance with the License.
+//   - You may obtain a copy of the License at
+//     *
+//   - http://www.apache.org/licenses/LICENSE-2.0
+//     *
+//   - Unless required by applicable law or agreed to in writing, software
+//   - distributed under the License is distributed on an "AS IS" BASIS,
+//   - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   - See the License for the specific language governing permissions and
+//   - limitations under the License.
+//     */
+package cmd
+
+// generate a test for version, and test the version command is rhino version
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVersionCommand(t *testing.T) {
+	cmd := NewVersionCommand()
+	buf := new(bytes.Buffer)
+	cmd.SetOut(buf)
+	err := cmd.Execute()
+
+	assert.NoError(t, err)
+
+	output := buf.String()
+	expected := "OpenRHINO v0.2.0\nOpen MPI v4.1.5"
+	assert.Contains(t, output, expected, "expected output to contain %q, but got %q", expected, output)
+}
